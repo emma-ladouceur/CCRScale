2f3cb867c4c627e51a7d009edab680f2a466c2ad
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-12-23 15:34:01 +0100

    alpha try again

--- alpha.rich.R
+++ alpha.rich.R
@@ -11,20 +11,16 @@ path <- '/gpfs1/data/idiv_chase/emmala/CCRScale'
 alpha_dat <- read.csv(paste0(path, '/alpha_div.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 
 
-alpha_dat_np <- alpha_dat %>% filter(site_status == "never-plowed") %>% 
-  summarise(alpha_rich_p_np = mean(alpha_rich))
-
-alpha_dat_of <- alpha_dat %>% filter(site_status == "old field") %>%  
-  summarise(alpha_rich_p = mean(alpha_rich))
-
 alpha_dat_of <- alpha_dat %>% filter(site_status == "old field") 
 
-alpha_dat_of$alpha_rich_p<-(alpha_dat_of$alpha_rich/9.21 * 100)
+alpha_dat_of$alpha_rich_p <- (alpha_dat_of$alpha_rich/9.21 *100)
+
 
 alpha_dat_of$YSA <- as.numeric(alpha_dat_of$YSA)
 alpha_dat_of$log_alpha_rich_p <- log(alpha_dat_of$alpha_rich_p)
 alpha_dat_of$log_YSA <- log(alpha_dat_of$YSA)
 alpha_dat_of$c.YSA<-alpha_dat_of$YSA-mean(alpha_dat_of$YSA)
+alpha_dat_of$Field<-as.factor(as.character(alpha_dat_of$Field))
 alpha_dat_of$Year<-as.factor(as.character(alpha_dat_of$Year))
 alpha_dat_of$Field<-as.factor(as.character(alpha_dat_of$Field))
 
