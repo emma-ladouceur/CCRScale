6a46f0306db200ad667762638afef500a5dd7b2f
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-15 14:03:31 +0200

    work path fix

--- cluster/gamma_pie_d.sh
+++ cluster/gamma_pie_d.sh
@@ -9,7 +9,7 @@
 #SBATCH -o /work/%u/%x-%j.out
 
 module load foss/2019b R/3.6.2-2
-export OFILE=/work/$USER/NutNet/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
+export OFILE=/work/$USER/CCRScale/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
 export LANG=en_US.UTF-8
 Rscript --vanilla /home/ladouceu/projects/CCRScale/cluster/gamma_pie_d.R
 
