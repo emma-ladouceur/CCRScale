18a5d5c096fef793975a80a533cf5a12a37df603
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-15 18:08:54 +0200

    gamma beta data

--- cluster/beta_pie_c.R
+++ cluster/beta_pie_c.R
@@ -9,7 +9,7 @@ gamma_dat <- read.csv(paste0(path, '/gamma_div.csv'), header=T,fill=TRUE,sep=","
 
 
 p.beta.spie <-  brm(log_beta_ENSPIE_p ~  log_YSA + (1 + log_YSA | Field) + (1 | Year),
-                   data = gamma_dat_of, family=student(),cores = 4, iter=10000,warmup=1000, chains = 4)
+                   data = gamma_dat, family=student(),cores = 4, iter=10000,warmup=1000, chains = 4)
 
 
 save(p.beta.spie,
--- cluster/gamma_pie_c.R
+++ cluster/gamma_pie_c.R
@@ -8,7 +8,7 @@ gamma_dat <- read.csv(paste0(path, '/gamma_div.csv'), header=T,fill=TRUE,sep=","
 
 
 p.gamma.spie<-  brm(log_gamma_ENSPIE_p ~  log_YSA  +  ( 1 + log_YSA  | Field) + (1 | Year),
-                    data = gamma_dat_of, control = list(adapt_delta = 0.99), cores = 4, iter=12000, warmup=1000, chains = 4)
+                    data = gamma_dat, control = list(adapt_delta = 0.99), cores = 4, iter=12000, warmup=1000, chains = 4)
 
 
 save(p.gamma.spie,
