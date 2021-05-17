831859136dd4c33385e405f04c951208ffdd5c17
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-17 22:28:53 +0200

    alpha

--- cluster/alpha_rich_c.R
+++ cluster/alpha_rich_c.R
@@ -8,7 +8,7 @@ alpha_dat <- read.csv(paste0(path, '/alpha_div_percent.csv'), header=T,fill=TRUE
 
 
 p.alpha.rich <-  brm(log_alpha_rich_p ~  log_YSA + ( 1 + log_YSA  | Field/Transect/Plot) + (1 | Year),
-                       data = alpha_dat_of, family=student(), cores = 4, iter=10000,warmup = 1000, control =
+                       data = alpha_dat, family=student(), cores = 4, iter=10000,warmup = 1000, control =
                          list(adapt_delta = 0.99), chains = 4)
 
 save(p.alpha.rich,
