e51039f465bbf38e2418a79b46b0cb843a3e3e09
Emma Ladouceur <emmala@gmail.com>
2021-09-14 13:49:31 +0200

    gamma 999

--- cluster/gamma_pie_c.R
+++ cluster/gamma_pie_c.R
@@ -9,7 +9,7 @@ gamma_dat <- read.csv(paste0(path, '/gamma_div_percent.csv'), header=T,fill=TRUE
 
 p.gamma.spie<-  brm(log_gamma_ENSPIE_p ~  log_YSA  +  ( 1 + log_YSA  | Field) + (1 | Year),
                     data = gamma_dat, cores = 4, iter=12000, warmup=1000, chains = 4,
-                    control = list(adapt_delta = 0.999))
+                    control = list(adapt_delta = 0.9999))
 
 
 save(p.gamma.spie,
